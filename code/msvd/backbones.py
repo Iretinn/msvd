7b47cdeb55a9bd7deccdc78eab65682575a005e4
Iretinn <96709787+Iretinn@users.noreply.github.com>
2024-09-03 16:25:57 +0800

    Update backbones.py

--- code/msvd/backbones.py
+++ code/msvd/backbones.py
@@ -1,5 +1,6 @@
 from transformers import RobertaConfig, RobertaModel
 
+
 def get_backbone(args):
     config = RobertaConfig.from_pretrained(args.config_name if args.config_name else args.model_name_or_path)
     encoder = RobertaModel.from_pretrained(args.model_name_or_path, config=config)
