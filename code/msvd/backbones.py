2e0c0a8c9a4334fef8a5f8e04239d377a455a3b8
Iretinn <96709787+Iretinn@users.noreply.github.com>
2024-09-03 16:24:59 +0800

    Update backbones.py

--- code/msvd/backbones.py
+++ code/msvd/backbones.py
@@ -1,7 +1,4 @@
-import torch.nn as nn
-from transformers import (WEIGHTS_NAME, get_linear_schedule_with_warmup,
-                          RobertaConfig, RobertaModel, RobertaTokenizer)
-
+from transformers import RobertaConfig, RobertaModel
 
 def get_backbone(args):
     config = RobertaConfig.from_pretrained(args.config_name if args.config_name else args.model_name_or_path)
